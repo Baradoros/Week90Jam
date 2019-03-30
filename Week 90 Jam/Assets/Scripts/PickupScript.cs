98e212d3f5618a6ffadebb325ccd41d7f509c868
lonewolfdesign <thomaswoodthorpe@gmail.com>
2019-03-30 16:44:13 +0000

    Pickup Place OBJ

--- Week 90 Jam/Assets/Scripts/PickupScript.cs
+++ Week 90 Jam/Assets/Scripts/PickupScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PickupScript : MonoBehaviour
 {
     public int isotopes;
+    public GameObject isotopeobj;
+    public Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +16,10 @@ public class PickupScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //HARD CODE ON PURPOSE! RAZ!
+        if(Input.GetKeyDown(KeyCode.G)){
+            Instantiate(isotopeobj, this.transform.position + offset, this.transform.rotation);
+        }
     }
     void OnCollisionEnter(Collision col){
         if(col.gameObject.tag == "isotope"){
