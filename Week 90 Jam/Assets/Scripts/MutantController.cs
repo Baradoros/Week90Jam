67ea4050a1126865b985a10ca342c2fa2ad9dd21
lonewolfdesign <thomaswoodthorpe@gmail.com>
2019-03-30 16:02:29 +0000

    New Movement System

--- Week 90 Jam/Assets/Scripts/MutantController.cs
+++ Week 90 Jam/Assets/Scripts/MutantController.cs
@@ -7,6 +7,8 @@ public class MutantController : MonoBehaviour
     public Rigidbody rb;
     public Vector3 direction;
     public float speed;
+    public int velz;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +19,13 @@ public class MutantController : MonoBehaviour
     void Update()
     {
         direction = new Vector3(0,0,0);
-        if(Input.GetKey(KeyCode.W)){
+        direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        /* 
+        if(Input.GetAxisRaw("Horizontal") == 0){
             direction += new Vector3(0,0,1);
         }
+        */
+        /* 
         if(Input.GetKey(KeyCode.S)){
             direction += new Vector3(0,0,-1);
         }
@@ -29,6 +35,7 @@ public class MutantController : MonoBehaviour
         if(Input.GetKey(KeyCode.A)){
             direction += new Vector3(-1,0,0);
         }
+        */
     }
     void FixedUpdate(){
         rb.transform.position += direction * speed * Time.deltaTime;
