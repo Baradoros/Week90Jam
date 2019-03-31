edd46224f350b7ce856a0007195c197fc2cf4208
Sean Lange <seanlange@The-Benatar.local>
2019-03-31 09:58:27 -0700

    updated Missions

--- Week 90 Jam/Assets/Missions/Objectives/ObjectiveData.cs
+++ Week 90 Jam/Assets/Missions/Objectives/ObjectiveData.cs
@@ -3,4 +3,6 @@
 [CreateAssetMenu(fileName = "Objective Data", menuName = "Objective Data")]
 public class ObjectiveData : ScriptableObject
 {
+    public string objectiveTitle;
+    public Sprite objectiveSprite;
 }
