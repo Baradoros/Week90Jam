5675aa9ff8e8aeb3a72ca0cad16b77b31e7f7ca7
Sean Lange <seanlange@The-Benatar.local>
2019-03-31 16:51:37 -0700

    carry something

---
+++ Week 90 Jam/Assets/Scripts/CarryObject.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class CarryObject : MonoBehaviour
+{
+    public LayerMask pickupableLayer;
+    public GameObject potentialHoldObject;
+    public GameObject objectHeld;
+
+    private void Update()
+    {        
+        if (Input.GetKeyUp(KeyCode.B))
+        {
+            if (objectHeld)
+            {
+                DropUpObject(objectHeld);
+                objectHeld = null;
+            }
+            
+            if (potentialHoldObject != null)
+            {
+                PickUpObject(potentialHoldObject);
+                objectHeld = potentialHoldObject;
+                potentialHoldObject = null;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        potentialHoldObject = other.gameObject;
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        potentialHoldObject = null;
+    }
+
+    private void PickUpObject(GameObject gObj)
+    {
+        gObj.transform.parent = transform.parent.transform;
+    }
+    
+    private void DropUpObject(GameObject gObj)
+    {
+        gObj.transform.parent = null;
+    }
+}
