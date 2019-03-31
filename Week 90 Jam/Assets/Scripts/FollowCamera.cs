a416f82af8efe094d13d36e9f97bf43ab5db4bfb
Baradoros <thatfunderburkguy@gmail.com>
2019-03-31 18:23:44 -0500

    Follow Camera

---
+++ Week 90 Jam/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;
+
+    [Range(0,1)]
+    public float smoothSpeed = 0.125f;
+
+    public Vector3 offset = new Vector3(0, 10, -10);
+
+    private Vector3 velocity = Vector3.zero;
+
+    private void FixedUpdate() {
+        // Position to move towards
+        Vector3 targetPosition = target.position + offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothSpeed);
+
+        // Sideways movement is not tracked with rotation. Looks better that way
+        transform.LookAt(new Vector3(transform.position.x, target.position.y, target.position.z));
+    }
+}
