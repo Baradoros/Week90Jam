4cfc42572321c5e8da78b267454bd6e928b1fd5e
Baradoros <thatfunderburkguy@gmail.com>
2019-04-01 17:05:12 -0500

    Added reactor to StationTest and Reactor Pressure script

---
+++ Week 90 Jam/Assets/Scripts/ReactorPressure.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactorPressure : MonoBehaviour
+{
+    [Range(0, 100)]
+    [SerializeField]
+    private int _pressure;
+    // Set this property from other scripts to alter pressure
+    public int pressure {
+        get {
+            return _pressure;
+                }
+        set {
+            // Ensure value is always 0 - 100
+            _pressure = Mathf.Clamp(value, 0, 100);
+
+            // If Delay() is already running, stop the current one before starting a new one
+            if (decayCountdownRuning) {
+                StopCoroutine(Delay());
+                decayCountdownRuning = false;
+            }
+            StartCoroutine(Delay());
+        }
+    }
+    public float decayRateInSeconds = 1.0f;
+
+    private bool decayCountdownRuning = true;
+    public bool isDecaying { get; private set; }
+
+
+    void Start()
+    {
+        isDecaying = false;
+        pressure = 50;
+    }
+
+    void Update()
+    {
+        if (isDecaying) {
+            StartCoroutine(Decay());
+        } else {
+            StopCoroutine(Decay());
+        }
+        
+    }
+
+    IEnumerator Decay() {
+        _pressure--; // Sets backing field directly to avoid starting the delay all over again
+        yield return new WaitForSeconds(decayRateInSeconds);
+        Decay();
+    }
+
+    IEnumerator Delay() {
+        decayCountdownRuning = true;
+        yield return new WaitForSeconds(6.0f);
+        decayCountdownRuning = false;
+        isDecaying = true;
+    }
+}
